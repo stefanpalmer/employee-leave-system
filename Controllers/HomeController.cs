e02153592d8a6a9cfbd7f770d819aea3d5f76894
Stefan Palmer <palmerstefan@yahoo.com>
2025-03-04 15:56:05 +0100

    Setup identity in project.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace EmployeeLeaveManagement.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return !User.Identity.IsAuthenticated ? this.Redirect("~/identity/account/login") : View();
         }
 
         public IActionResult Privacy()
